d52f91c4dda239a613e94df0d017934662be4736
Dan Levy <dan@danlevy.net>
2020-02-19 04:09:59 -0700

    great success, babel defeated! ✨

--- index.test.js
+++ index.test.js
@@ -1,8 +1,14 @@
-const SnakeGame = require('./')
+import SnakeGame from './main'
 
 describe('can initialize game', () => {
   it('can start a 10x10 game', () => {
-    const game = SnakeGame({ size: 10 })
-    game.move
+    const game = SnakeGame({ size: 10, output: 'ascii' })
+    let state = null
+    state = game.next().value
+    expect(state).toMatchSnapshot('initial')
+    state = game.next('left').value
+    expect(state).toMatchSnapshot('1-left')
+    state = game.next().value
+    expect(state).toMatchSnapshot('2-left-implicit')
   })
 })
--- main.js
+++ main.js
@@ -1,26 +1,54 @@
+const BODY_CELL = '#'
+
+const renderAsciiGrid = grid => {
+  return grid.map(row => row.join('')).join('\n')
+}
 
 // Make a pull-based renderer - i.e. generators
-export default function SnakeGame({size = 11}) {
-  const middleish = parseInt(size / 2.0, 10)
-  const grid = Array.from({length: size}, () => Array.from({length: size}, () => ' '))
+const getHeadIcon = ({ direction }) => {
+  if (direction === 'up') return `▲`
+  if (direction === 'down') return `▼`
+  if (direction === 'left') return `◀`
+  if (direction === 'right') return `▶︎`
+  return '🐍'
+}
+const getNextCell = (currentHead, direction) => {
+  if (direction === 'up') return { x: currentHead.x, y: currentHead.y - 1 }
+  if (direction === 'down') return { x: currentHead.x, y: currentHead.y + 1 }
+  if (direction === 'left') return { x: currentHead.x - 1, y: currentHead.y }
+  if (direction === 'right') return { x: currentHead.x + 1, y: currentHead.y }
+  throw Error('Invalid direction: ' + direction)
+}
+
+
+export default function* SnakeGame({ size = 11, output = 'ascii' }) {
+  const centerStart = (size / 2.0).toFixed(0)
+  const grid = Array.from({ length: size }, () => Array.from({ length: size }, () => ' '))
   // note: cells records the sequence of steps, might be overkill, let's get back to tests
-  const snake = { direction: 'up', cells: [{x: middleish, y: middleish}] }
+  const snake = { direction: 'up', cells: [{ x: centerStart, y: centerStart }] }
 
-  const getCellIcon = (coords, index) => index === 0 ? '🐍' : '#' //'＃'
-  const getNextCell = (currentHead, direction) => {
-    if (direction === 'up') return { x: currentHead.x, y: currentHead.y - 1 }
-    if (direction === 'down') return { x: currentHead.x, y: currentHead.y + 1 }
-    if (direction === 'left') return { x: currentHead.x - 1, y: currentHead.y }
-    if (direction === 'right') return { x: currentHead.x + 1, y: currentHead.y }
-    if (direction === 'up') return { x: currentHead.x, y: currentHead.y - 1 }
-  }
-  return {
-    *move(newDirection = snake.direction) {
-      const currentHead = snake.cells[0]
+  // set the start position
+  grid[centerStart][centerStart] = getHeadIcon({ direction: snake.direction })
 
-      snake.cells
-    }
+  // const getCellIcon = (coords, index) => index === 0 ? '🐍' : BODY_CELL //'＃'
+  const getGameState = () => output === 'ascii' ? renderAsciiGrid(grid) : JSON.parse(JSON.stringify({ grid, snake }))
+
+  while (true) {
+    const nextDirection = yield getGameState()
+    const prevHead = snake.cells[0]
+    snake.cells.unshift(getNextCell(prevHead, nextDirection || snake.direction))
+    const currHead = snake.cells[0]
+    grid[prevHead.y][prevHead.x] = BODY_CELL
+    grid[currHead.y][currHead.x] = getHeadIcon({ direction: snake.direction })
   }
+  // return {
+  //   *move(newDirection = snake.direction) {
+
+  //     const currentHead = snake.cells[0]
+
+  //     snake.cells
+  //   }
+  // }
 }
 
 // Need a func to:
